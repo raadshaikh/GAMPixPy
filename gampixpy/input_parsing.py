cea39c8b12915f534a964827463b48506e1165ad
Daniel Douglas <dougl215@slac.stanford.edu>
2024-11-08 16:49:44 -0800

    implemented edep-sim (hdf5) input parsing

--- gampixpy/input_parsing.py
+++ gampixpy/input_parsing.py
@@ -1,41 +1,100 @@
-from tracks import Track, Segment
+from .tracks import Track, Segment
+
+import numpy as np
 
 class InputParser:
     def __init__(self, input_filename, sequential_sampling = True):
+        print ("init parent class")
         self.input_filename = input_filename
 
         self.sampling_order = []
-        self.sample_cursor = 0
-        
+
+        self.open_file_handle()
         self.generate_sample_order(sequential_sampling)
-
-    def generate_sample_order(sequential_sampling):
-        # implement!
-        # page through the file, enumerate the number of samples,
-        # and generate a list of indices
-        # if sequential_sampling, this should just be range(n_samples)
         
-        self.sampling_order = []
-
-    def get_sample(self, index):
-        # dummy method to be replaced by subclasses
-        
-        return None
-        
-    def get_next_sample(self):
+    def __iter__(self):
         for sample_index in self.sampling_order:
             yield self.get_sample(sample_index)
 
 class PenelopeParser (InputParser):
-    def get_penelope_sample(self, sample_index):
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    
+    def open_file_handle(self):
+        # import h5py
+        # self.file_handle = h5py.File(self.input_filename)
+        return 
+
+    def generate_sample_order(self, sequential_sampling):
+        # n_images_per_file = len(np.unique(self.file_handle['trajectories']['eventID']))
+        return 
+        
+    def get_penelope_sample(self):
+        print ("wowzers")
+        print (self.input_filename)
         # do the magic that lets you read from a penelope file
+        return None
 
     def get_sample(self, index):
         return self.get_penelope_sample(index)
 
 class RooTrackerParser (InputParser):
     def get_G4_sample(self, sample_index):
+        return None
         # do the magic that lets you read from a Geant4 ROOT file
 
     def get_sample(self, index):
         return self.get_G4_sample(index)
+
+class EdepSimParser (InputParser):
+    def open_file_handle(self):
+        import h5py
+        self.file_handle = h5py.File(self.input_filename)
+
+    def generate_sample_order(self, sequential_sampling):
+        n_images_per_file = len(np.unique(self.file_handle['trajectories']['eventID']))
+        
+    def get_edepsim_event(self, sample_index):
+        segment_mask = self.file_handle['segments']['eventID'] == sample_index
+        event_segments = self.file_handle['segments'][segment_mask]
+
+        trajectory_mask = self.file_handle['trajectories']['eventID'] == sample_index
+        event_trajectories = self.file_handle['trajectories'][trajectory_mask]
+
+        charge_per_segment = self.do_recombination(event_segments)
+        charge_points, charge_values = self.do_point_sampling(event_segments, charge_per_segment)
+        
+        return Track(charge_points, charge_values)
+
+    def do_recombination(self, segments):
+        charge_yield_per_energy = 1 # just do a constant factor right now
+    
+        return segments['dE']*charge_yield_per_energy
+    
+    def do_point_sampling(self, segments, charge_per_segment):
+        # point sampling with a fixed number of samples per length
+        # it may be faster to do sampling another way (test in future!)
+        #  - sample with fixed amount of charge
+        #  - sample with fixed number of samples per segment
+
+        sample_density = 1.e4 # samples per unit length
+        start_vec = np.array([segments['x_start'],
+                              segments['y_start'],
+                              segments['z_start']])
+        end_vec = np.array([segments['x_end'],
+                            segments['y_end'],
+                            segments['z_end']])
+        segment_dirs = end_vec - start_vec
+
+        samples_per_segment = (segments['dx']*sample_density).astype(int)
+
+        sample_positions = np.concatenate([(start_vec[:,i,None] + np.linspace(0, 1, samples_per_segment[i])*segment_dirs[:,i,None]).T
+                                           for i in range(len(samples_per_segment))])
+
+        sample_charges = np.concatenate([samples_per_segment[i]*[charge_per_segment[i]/samples_per_segment[i]]
+                                         for i in range(len(samples_per_segment))])
+        
+        return sample_positions, sample_charges
+
+    def get_sample(self, index):
+        return self.get_edepsim_event(index)
--- gampixpy/tracks.py
+++ gampixpy/tracks.py
@@ -1,13 +1,15 @@
 class Track:
-    def __init__(self, segment_list):
-        self.depositions = segment_list
+    def __init__(self, sample_positions, sample_charges):
+        self.positions = sample_positions
+        self.charges = sample_charges
 
-        self.charge_unit = 0
-        self.charge_samples = []
-        self.drifted_samples = []
+        self.drifted_positions = []
+        self.drifted_charges = []
 
-class Segment:
-    def __init__(self, start_position, end_position, energy):
-        self.start_position = start_position
-        self.end_position = end_position
-        self.ionization_energy = energy
+# this seems to be relevant only for specific inputs
+# let's not use this for now
+# class Segment:
+#     def __init__(self, start_position, end_position, energy):
+#         self.start_position = start_position
+#         self.end_position = end_position
+#         self.ionization_energy = energy
