fbcf5e8a06199743a30285d5f4ae9ebd3b51efc2
Daniel Douglas <dougl215@slac.stanford.edu>
2025-03-22 03:33:36 -0700

    tweaks to plotting tools

--- gampixpy/plotting.py
+++ gampixpy/plotting.py
@@ -113,7 +113,63 @@ class EventDisplay:
         self.ax.set_xlabel(r'x (transverse)')
         self.ax.set_ylabel(r'y (transverse)')
         self.ax.set_zlabel(r'z (drift)')
+
+    def plot_coarse_tile_measurement_timeline(self, readout_config):
+        # TODO: implement!
+
+        xlim = []
+        ylim = []
+        zlim = []
+        
+        for this_hit in self.track_object.coarse_tiles_samples:
+            cell_center_xy = this_hit.coarse_cell_pos
+            # cell_center_z = this_hit.coarse_measurement_time
+            cell_trigger_t = this_hit.coarse_measurement_time
+            cell_measurement = this_hit.coarse_cell_measurement
+
+            x_bounds = [cell_center_xy[0] - 0.5*readout_config['coarse_tiles']['pitch'],
+                        cell_center_xy[0] + 0.5*readout_config['coarse_tiles']['pitch']]
+            y_bounds = [cell_center_xy[1] - 0.5*readout_config['coarse_tiles']['pitch'],
+                        cell_center_xy[1] + 0.5*readout_config['coarse_tiles']['pitch']]
+            z_bounds = [cell_trigger_t,
+                        cell_trigger_t + readout_config['coarse_tiles']['clock_interval']*readout_config['coarse_tiles']['integration_length']]
             
+            bottom_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0]],
+                                    [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                    [z_bounds[0], z_bounds[0], z_bounds[0], z_bounds[0], z_bounds[0]]]).T
+            top_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0]],
+                                 [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                 [z_bounds[1], z_bounds[1], z_bounds[1], z_bounds[1], z_bounds[1]]]).T
+            left_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[0], x_bounds[0], x_bounds[0]],
+                                  [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                  [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            right_face = np.array([[x_bounds[1], x_bounds[1], x_bounds[1], x_bounds[1], x_bounds[1]],
+                                   [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                   [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            back_face = np.array([[x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0], x_bounds[0]],
+                                  [y_bounds[0], y_bounds[0], y_bounds[0], y_bounds[0], y_bounds[0]],
+                                  [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            front_face = np.array([[x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0], x_bounds[0]],
+                                   [y_bounds[1], y_bounds[1], y_bounds[1], y_bounds[1], y_bounds[1]],
+                                   [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            faces = [bottom_face,
+                     top_face,
+                     left_face,
+                     right_face,
+                     back_face,
+                     front_face,
+                     ]
+            # self.ax.scatter(this_hit.coarse_cell_id[0],
+            #                 this_hit.coarse_cell_id[1],
+            #                 this_hit.coarse_measurement_time,
+            #                 c = this_hit.coarse_cell_measurement,
+            #                 )
+            self.ax.add_collection3d(Poly3DCollection(faces, facecolors = 'cyan', linewidths=1, edgecolors = 'k', alpha = 0.25))
+            
+        self.ax.set_xlabel(r'x (transverse)')
+        self.ax.set_ylabel(r'y (transverse)')
+        self.ax.set_zlabel(r'z (drift)')
+        
     def plot_pixel_measurement(self, readout_config):
         
         for this_hit in self.track_object.pixel_samples:
@@ -128,6 +184,63 @@ class EventDisplay:
                         cell_center_xy[1] + 0.5*readout_config['pixels']['pitch']]
             z_bounds = [cell_center_z,
                         cell_center_z + readout_config['pixels']['z_bin_width']*readout_config['pixels']['integration_length']]
+            
+            
+            bottom_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0]],
+                                    [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                    [z_bounds[0], z_bounds[0], z_bounds[0], z_bounds[0], z_bounds[0]]]).T
+            top_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0]],
+                                 [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                 [z_bounds[1], z_bounds[1], z_bounds[1], z_bounds[1], z_bounds[1]]]).T
+            left_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[0], x_bounds[0], x_bounds[0]],
+                                  [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                  [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            right_face = np.array([[x_bounds[1], x_bounds[1], x_bounds[1], x_bounds[1], x_bounds[1]],
+                                   [y_bounds[0], y_bounds[1], y_bounds[1], y_bounds[0], y_bounds[0]],
+                                   [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            back_face = np.array([[x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0], x_bounds[0]],
+                                  [y_bounds[0], y_bounds[0], y_bounds[0], y_bounds[0], y_bounds[0]],
+                                  [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            front_face = np.array([[x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0], x_bounds[0]],
+                                   [y_bounds[1], y_bounds[1], y_bounds[1], y_bounds[1], y_bounds[1]],
+                                   [z_bounds[0], z_bounds[0], z_bounds[1], z_bounds[1], z_bounds[0]]]).T
+            faces = [bottom_face,
+                     top_face,
+                     left_face,
+                     right_face,
+                     back_face,
+                     front_face,
+                     ]
+            self.ax.add_collection3d(Poly3DCollection(faces,
+                                                      facecolors = 'orange',
+                                                      # facecolors = [np.log10(this_hit.hit_measurement)/10, 0, 0],
+                                                      linewidths=1,
+                                                      edgecolors = 'k',
+                                                      alpha = 0.25))
+            
+        self.ax.set_xlabel(r'x (transverse)')
+        self.ax.set_ylabel(r'y (transverse)')
+        self.ax.set_zlabel(r'z (drift)')
+
+    def plot_pixel_measurement_timeline(self, readout_config):
+        
+        for this_hit in self.track_object.pixel_samples:
+            cell_center_xy = this_hit.pixel_pos
+            # cell_center_z = this_hit.hit_timestamp
+            cell_trigger_t = this_hit.hit_timestamp
+            cell_measurement = this_hit.hit_measurement
+
+            # plt.scatter(cell_center_xy[0],
+            #             cell_center_xy[1],
+            #             cell_trigger_t,
+            #             alpha = 0)
+
+            x_bounds = [cell_center_xy[0] - 0.5*readout_config['pixels']['pitch'],
+                        cell_center_xy[0] + 0.5*readout_config['pixels']['pitch']]
+            y_bounds = [cell_center_xy[1] - 0.5*readout_config['pixels']['pitch'],
+                        cell_center_xy[1] + 0.5*readout_config['pixels']['pitch']]
+            z_bounds = [cell_trigger_t,
+                        cell_trigger_t + readout_config['pixels']['clock_interval']*readout_config['pixels']['integration_length']]
             
             
             bottom_face = np.array([[x_bounds[0], x_bounds[0], x_bounds[1], x_bounds[1], x_bounds[0]],
